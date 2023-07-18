220a13d40a8cbce86955bd5fa929c25fb73b6d22
Promapi <matyasbelayneh@gmail.com>
2023-07-18 12:18:05 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,14 @@
  *     **/
 int main(void)
 {
-	char c[] = "_putchar";
-	int i;
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(c[i]);
-	}
- 	return (0):
+        char c[] = "_putchar";
+        int i;
+
+        for (i = 0; i < 8; i++)
+        {
+	                _putchar(c[i]);
+	        }
+        _putchar('\n');
+        return (0);
+
 }
