d7bdcc86fb96f57e390cb56f10e126c3d9edcae0
Promapi <matyasbelayneh@gmail.com>
2023-07-18 23:48:36 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
- * main - entry block
- * @void: no argument
- * Return: 0
+ * main - The entry point of the program
+ *
+ * Return: Always 0
  **/
 int main(void)
 {
-	char c[] = "_putchar";
-	int i;
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+    char c[] = "_putchar";
+    int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(c[i]);
+    }
+
+    _putchar('\n');
+
+    return (0);
 }
